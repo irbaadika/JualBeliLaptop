0a0c61a14004a18fe93fd7a02267565b701998d9
tzelginianc <tzelginia78@gmail.com>
2022-11-03 18:12:17 +0700

    loginController

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('register', [
+            'title' => 'Register'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $ValidatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'phone' => 'required|numeric',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:8'
+        ]);
+
+        $ValidatedData['password'] = bcrypt($ValidatedData['password']);
+
+        User::create($ValidatedData);
+        if (Auth::attempt($ValidatedData)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+        }
+        // $request->session()->flash('success','Registrasi berhasil, silahkan Login');
+        return redirect('/login')->with('success', 'Registrasi berhasil');
+    }
+}
